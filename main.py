dfb0956df966734e89033cca4a4f04788d700c80
stacygathu <gathustacy1gmail.com>
2025-07-06 20:06:25 +0300

    langchain to langchain_community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
